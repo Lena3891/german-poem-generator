b8c43e49573909c20bfb0e8b3aa648d2fe786a8e
Lena3891 <164846971+Lena3891@users.noreply.github.com>
2024-04-25 18:12:46 +0200

    changed name and added axios and api

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
+new Typewriter("#poem", {
+  strings: `Generating the poem here...`,
+  autoStart: true,
+});
+
+let form = document.querySelector("#poem-generator-form");
+
 function generatePoem(event) {
-    event.preventDefault();
+  event.preventDefault();
+  let instructions = document.querySelector(".instructions");
 
-   
+  let apiKey = "do37btb04e66032f8eb1ab0493255777";
+  let context =
+    "You are a poets and poem expert, please carefully follow the instructions.";
+  let prompt = `Write a poem about the topic: ${instructions.value}?`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-   new Typewriter("#poem", {
-     strings: "Wie lange mussten wir abwarten",
-     autoStart: true,
-     delay: 1,
-     cursor: "",
-   });
-  
+  let poemElement = document.querySelector("#poem");
+  poemElement.innerHTML = `<span class="blink">⏳ Generating poem about ${instructions.value}</span>`;
 
+  function displayPoem(response) {
+    new Typewriter("#poem", {
+      strings: `${response.data.answer}`,
+      autoStart: true,
+      delay: 1,
+      cursor: "",
+    });
+  }
+  axios.get(apiUrl).then(displayPoem);
 }
 
-let poemFormElement = document.querySelector("#poem-generator-form");
-poemFormElement.addEventListener("submit", generatePoem);
+form.addEventListener("submit", generatePoem);
